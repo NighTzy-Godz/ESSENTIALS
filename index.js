43cdbea678014d64cea66beb2ff2e71b72ab71c4
Aser James <ajhubero16@gmail.com>
2021-07-29 13:02:58 +0800

    5th

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+// -------- MAIN HOME PAGE ----------------
+
 let opener = document.querySelector("#openHead");
 
 let headUL = document.querySelector("ul");
 let search = document.querySelector(".search");
 
 opener.addEventListener("click", () => {
-  toggle();
+  mainToggle();
 });
 
-function toggle() {
+function mainToggle() {
   headUL.classList.toggle("show");
   search.classList.toggle("show");
+}
+
+// ---------------- MENU PAGE -------------------
+
+let sideOpen = document.querySelector("#sideOpen");
+let sideClose = document.querySelector("#sideClose");
+let sideBar = document.querySelector(".sideBar");
+
+sideOpen.addEventListener("click", () => {
+  sideToggle();
+});
+
+sideClose.addEventListener("click", () => {
+  sideToggle();
+});
+
+function sideToggle() {
+  sideBar.classList.toggle("sideShow");
+  sideOpen.classList.toggle("sideClose");
+  sideClose.classList.toggle("sideOpen");
 }
